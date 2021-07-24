bd387b76bea216d70f0bb9d59bf26f64a0e015a6
Ly Xuan Sang <sang.lxuan@gmail.com>
2021-07-24 22:54:02 +0700

    fix token name and symbol

--- conf/config.mainnet.js
+++ conf/config.mainnet.js
@@ -603,8 +603,8 @@ const mainnetBeneficiaries = [
 ];
 
 const token = {
-  name: "Spores Token 23",
-  symbol: "SPO23",
+  name: "Spores",
+  symbol: "SPO",
   decimals: 18,
   cap: "5000000000000000000000000000",
 };
